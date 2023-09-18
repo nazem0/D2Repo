28ba06d0858578c6a92b2b4716a2b406e0098751
mohamed <Mohamedsayed0097@gmail.com>
2023-09-18 14:03:54 +0300

    user order module assembly y nazem

---
+++ UserOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class UserOrder
+{
+    public static void Display()
+    {
+        User user = new User();
+        user.Id = 1;
+        user.Name = "Test2";
+        Order order = new Order();
+        order.Id = 1;
+        order.Price = 20;
+        order.User=user;
+        Console.WriteLine(order);
+    }	
+}
