596d4822bf5de43fe68082b94f819855ebba041d
nazem0 <mohamednazem09@gmail.com>
2023-09-18 13:03:12 +0300

    Added User Module

---
+++ UserModule.cs
@@ -0,0 +1,10 @@
+﻿public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id}, {Name}";
+    }
+}
